45244331e27c549eefefffd443b2b3317303b542
juan carlos <juancarlosmz547@gmail.com>
2023-01-22 17:37:37 -0500

    models_users

---
+++ main.py
@@ -0,0 +1,44 @@
+from uuid import UUID
+from datetime import date 
+
+from fastapi import FastAPI
+from pydantic import BaseModel, EmailStr, Field
+from typing import Optional
+
+ 
+app = FastAPI()
+
+#Models
+
+class UserBase(BaseModel):
+    pass
+
+class UserLogin(UserBase):
+    password : str =Field(
+        ...,
+        min_length = 8
+    )
+
+class User(UserBase):
+    user_id:  UUID = Field(...) #universal unique identifier 
+    email: EmailStr = Field(...)
+    password : str =Field(
+        ...,
+        min_length = 8
+    )
+    first_name: str = Field(
+        ...,
+        min_length = 1,
+        max_length = 50
+    )
+    birth_date: Optional[date] = Field(default= None)
+
+class Tweet(BaseModel):
+    pass
+
+@app.get(
+    path="/")
+def home():
+    return {
+        "twitter API": "Working!"
+    }
